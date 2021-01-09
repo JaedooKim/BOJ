620918cba9900eb6fb221d1307ce059b69adf6c3
김재두 <dev.jdkim@gmail.com>
2021-01-09 19:13:35 +0900

    [10845] 큐

--- algorithm/src/p_10845.java
+++ algorithm/src/p_10845.java
@@ -1,8 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+
 public class p_10845 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(in.readLine());
+        LinkedList queue = new LinkedList();
+        StringBuilder sb = new StringBuilder();
+        String[] command;
 
-    // TODO: 2021. 1. 7.
-    //  큐 문제 풀기
-    public static void main(String[] args) {
-
+        while (n-- > 0) {
+            command = in.readLine().split(" ");
+            switch (command[0]) {
+                case "push" : queue.push(command[1]);
+                    break;
+                case "pop" :
+                    if (queue.isEmpty()) {
+                        sb.append(-1+"\n");
+                    } else {
+                        sb.append(queue.getLast()+"\n");
+                        queue.removeLast();
+                    }
+                    break;
+                case "size" :
+                    sb.append(queue.size()+"\n");
+                    break;
+                case "empty" :
+                    if (queue.isEmpty()) {
+                        sb.append(1+"\n");
+                    } else {
+                        sb.append(0+"\n");
+                    }
+                    break;
+                case "front" :
+                    if (queue.isEmpty()) {
+                        sb.append(-1+"\n");
+                    } else {
+                        sb.append(queue.getLast()+"\n");
+                    }
+                    break;
+                case "back" :
+                    if (queue.isEmpty()) {
+                        sb.append(-1+"\n");
+                    } else {
+                        sb.append(queue.getFirst() + "\n");
+                    }
+                    break;
+                default:
+                    System.out.println("invalid input error.");
+            }
+        }
+        System.out.println(sb);
     }
 }

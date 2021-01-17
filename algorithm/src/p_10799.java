4eeb96462104f50ae50934df6c965adf4b1bd82f
김재두 <dev.jdkim@gmail.com>
2021-01-17 16:53:11 +0900

    [10799] 쇠막대기

--- algorithm/src/p_10799.java
+++ algorithm/src/p_10799.java
@@ -1,6 +1,28 @@
-public class p_10799 {
-    public static void main(String[] args) {
-        // TODO: 쇠막대기 레이저로 자르기
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
 
+public class p_10799 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        Stack<Integer> stack = new Stack<>();
+        int count = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+            if (input.substring(i,i+1).equals("(")) {
+                stack.push(i);
+            } else {
+                if (stack.peek() == i-1) {
+                    stack.pop();
+                    count+=stack.size();
+                } else {
+                    stack.pop();
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
     }
 }

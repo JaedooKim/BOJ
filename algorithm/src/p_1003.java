34dc96d02b959e0128688a6395b3b99069aa52d9
김재두 <dev.jdkim@gmail.com>
2021-08-09 22:04:06 +0900

    [1003] 피보나치 함수 + dp 풀이 추가

--- algorithm/src/p_1003.java
+++ algorithm/src/p_1003.java
@@ -3,9 +3,42 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class p_1003 {
+    /**
+     * dp 버전 풀이
+     *
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer sb = new StringBuffer();
 
-    static int zero = 0, one = 0;
+        int T = Integer.parseInt(br.readLine());
 
+        for (int i = 0; i < T; i++) {
+            int N = Integer.parseInt(br.readLine());
+            int[][] dp = new int[N + 1][2];
+
+            for (int j = 0; j <= N; j++) {
+                if (j == 0) {
+                    dp[j][0] = 1;
+                    dp[j][1] = 0;
+                } else if (j == 1) {
+                    dp[j][0] = 0;
+                    dp[j][1] = 1;
+                } else {
+                    dp[j][0] = dp[j - 1][0] + dp[j - 2][0];
+                    dp[j][1] = dp[j - 1][1] + dp[j - 2][1];
+                }
+
+            }
+            sb.append(dp[N][0] + " " + dp[N][1] + "\n");
+        }
+        System.out.println(sb.toString());
+    }
+
+
+    /*
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
@@ -33,5 +66,7 @@ public class p_1003 {
             System.out.printf("%d %d\n", zero[arr[i]], one[arr[i]]);
         }
     }
+
+     */
 
 }
